9cceaf93a34dc1e9a4ca1816e3a86057fa04d3ec
Davi Carneiro <davicarneirodcc@gmail.com>
2025-12-23 23:02:24 -0300

    feat: cv, resources

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,6 +10,19 @@ const blogCollection = defineCollection({
     })
 })
 
+const resourcesCollection = defineCollection({
+    type: 'data',
+    schema: () => z.object({
+        url: z.string().url(),
+        overrideTitle: z.string().optional(),
+        overrideDescription: z.string().optional(),
+        overrideImage: z.string().url().optional(),
+        tags: z.array(z.string()).optional(),
+        date: z.string().or(z.date()).optional(),
+    })
+})
+
 export const collections = {
     'blog': blogCollection,
+    'resources': resourcesCollection,
 };
